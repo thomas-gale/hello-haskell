ea4bf91bd432f4a77353defcb78ffdeb39c85688
Thomas Gale <gale.thomasjames@gmail.com>
2021-11-24 20:38:02 +0000

    Experimenting with my own recusive data type.

--- src/LearnLib.hs
+++ src/LearnLib.hs
@@ -12,6 +12,8 @@ module LearnLib
     square,
     area,
     isAllDigits,
+    BTree (..),
+    tree1,
   )
 where
 
@@ -24,7 +26,8 @@ doubleMe x = x + x
 
 doubleUs x y = x * 2 + y * 2
 
--- declare types (abbreviation/synonym) - must always begin with capital letter.
+-- declare type alias (abbreviation/synonym) - must always begin with capital letter.
+-- is a composition of already existing types.
 type Pos n = (n, n)
 
 origin :: Pos Int
@@ -60,3 +63,9 @@ area (Rect x y) = x * y
 isAllDigits :: [Char] -> Bool
 isAllDigits "" = False
 isAllDigits val = all ((== True) . (\x -> x `elem` ['1', '2', '3', '4', '5', '6', '7', '8', '9'])) val
+
+-- Recursive data types.
+data BTree a = Empty | BTree a (BTree a) (BTree a)
+  deriving (Show)
+
+tree1 = BTree 2 (BTree 7 (BTree 2 Empty Empty) (BTree 6 Empty Empty)) (BTree 5 Empty (BTree 9 (BTree 4 Empty Empty) Empty))
