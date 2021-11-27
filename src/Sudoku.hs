f3fde2d0b698f4414f3717882adcab99303d9135
Thomas Gale <gale.thomasjames@gmail.com>
2021-11-27 16:54:48 +0000

    Completed jotting the notes on the sudoku solver. It can now solve any puzzle!

--- src/Sudoku.hs
+++ src/Sudoku.hs
@@ -11,17 +11,37 @@ module Sudoku
     easy,
     gentle,
     diabolical,
+    unsolvable,
+    minimal,
+    blank,
     rows,
     cols,
     boxs,
     nodups,
+    valid,
     Choices,
     choices,
     cp,
     collapse,
+    solve,
+    minus,
+    reduce,
+    prune,
+    solve2,
+    fix,
+    solve3,
+    complete,
+    void,
+    consistent,
+    safe,
+    blocked,
+    solve4,
+    search,
+    expand,
   )
 where
 
+import qualified Data.Functor as Notes
 import Data.List
 
 -- Basic declarations
@@ -87,6 +107,37 @@ diabolical =
     ".54..8.7."
   ]
 
+unsolvable :: Grid
+unsolvable =
+  [ "1..9.7..3",
+    ".8.....7.",
+    "..9...6..",
+    "..72.94..",
+    "41.....95",
+    "..85.43..",
+    "..3...7..",
+    ".5.....4.",
+    "2..8.6..9"
+  ]
+
+minimal :: Grid
+minimal =
+  [ ".98......",
+    "....7....",
+    "....15...",
+    "1........",
+    "...2....9",
+    "...9.6.82",
+    ".......3.",
+    "5.1......",
+    "...4...2."
+  ]
+
+blank :: Grid
+blank = replicate n (replicate n '.')
+  where
+    n = boxsize ^ 2
+
 -- Extracting rows, columns and boxes.
 rows :: Matrix a -> [Row a]
 -- id is same as fn x = x
@@ -111,6 +162,12 @@ nodups :: Eq a => [a] -> Bool
 nodups [] = True
 nodups (x : xs) = x `notElem` xs && nodups xs
 
+valid :: Grid -> Bool
+valid g =
+  all nodups (rows g)
+    && all nodups (cols g)
+    && all nodups (boxs g)
+
 -- Basic solver section
 type Choices = [Value]
 
@@ -124,5 +181,81 @@ cp :: [[a]] -> [[a]]
 cp [] = [[]]
 cp (xs : xss) = [y : ys | y <- xs, ys <- cp xss]
 
+-- turn matrix of choices into a list of matrices
 collapse :: Matrix [a] -> [Matrix a]
 collapse = cp . map cp
+
+solve :: Grid -> [Grid]
+solve = filter valid . collapse . choices
+
+-- pruning the search space
+minus :: Choices -> Choices -> Choices
+-- (\\) operator is the list difference (non associative) from Data.List package
+-- [1,2,3] \\ [2,3] = [1]
+xs `minus` ys = if single xs then xs else xs \\ ys
+
+-- Remove any single values from the multiple other multiple choices in a row
+-- reduce ["3", "1", "123"] -> ["3", "1", "2"]
+reduce :: Row Choices -> Row Choices
+reduce xss = [xs `minus` singles | xs <- xss]
+  where
+    singles = concat (filter single xss)
+
+prune :: Matrix Choices -> Matrix Choices
+prune = pruneBy boxs . pruneBy cols . pruneBy rows
+  where
+    -- The f . something . f is the nice property that rows/cols/boxs have which is that rows . rows = id
+    pruneBy f = f . map reduce . f
+
+-- solve 2 (second iteration) - still not infeasible
+solve2 :: Grid -> [Grid]
+solve2 = filter valid . collapse . prune . choices
+
+-- repeated pruning
+fix :: Eq a => (a -> a) -> a -> a
+fix f x = if x == x' then x else fix f x'
+  where
+    x' = f x
+
+-- solve 3 - works for each, still too many choices for harder puzzles
+solve3 :: Grid -> [Grid]
+solve3 = filter valid . collapse . fix prune . choices
+
+-- further properties to achieve final solution algorithm
+complete :: Matrix Choices -> Bool
+complete = all (all single)
+
+void :: Matrix Choices -> Bool
+void = any (any null)
+
+-- check that row does not contain more that one same value of a single choice (otherwise sudoku is not solvable)
+consistent :: Row Choices -> Bool
+consistent = nodups . concat . filter single
+
+-- safe runs consistent over all rows/cols/boxs
+safe :: Matrix Choices -> Bool
+safe mc = all consistent (rows mc) && all consistent (cols mc) && all consistent (boxs mc)
+
+-- put checks together, matrix is blocked if void or not safe
+blocked :: Matrix Choices -> Bool
+blocked m = void m || not (safe m)
+
+-- Putting final solve together
+solve4 :: Grid -> [Grid]
+solve4 = search . prune . choices
+
+search :: Matrix Choices -> [Grid]
+search m
+  | blocked m = []
+  | complete m = collapse m
+  | otherwise = [g | m' <- expand m, g <- search (prune m')]
+
+-- Behaves the same as collapse except only collapses the first square with more than one choice.
+-- Notes.
+-- List span: applied to a predicate p and a list xs, returns a tuple where first element is longest prefix
+-- (possibly empty) of xs of elements that satisfy p and second element is the remainder of the list
+expand :: Matrix Choices -> [Matrix Choices]
+expand m = [rows1 ++ [row1 ++ [c] : row2] ++ rows2 | c <- cs]
+  where
+    (rows1, row : rows2) = span (all single) m
+    (row1, cs : row2) = span single row
