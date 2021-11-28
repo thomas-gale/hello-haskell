4ea8886faf694b64beb6bab768e39b6d8a62817e
Thomas Gale <gale.thomasjames@gmail.com>
2021-11-28 17:37:19 +0000

    Did a few more of the Functor/Applicative exercises.

--- src/Monads.hs
+++ src/Monads.hs
@@ -1,5 +1,6 @@
-module Monads (Tree (..)) where
+module Monads (Tree (..), ZipList (..)) where
 
+import qualified Control.Applicative as Control
 import Control.Monad
 
 -- 12.5 exercises
@@ -23,11 +24,26 @@ instance Functor Tree where
 -- 3. Define an instance of the applicative class for partially applied function type (a ->).
 -- Commented due to conflict with Base lib
 -- instance Applicative ((->) a) where
--- pure :: b -> (a -> b) (K combinator I think)
+-- pure :: b -> (a -> b) (K combinator I think) (also the type is b -> a -> b as the by default it associates to the right)
 -- pure f _ = f
 -- pure = const
 
 -- (<*>) :: (a -> b -> c) -> (a -> b) -> (a -> c) (S combinator I think)
 -- f <*> g = \x -> f x (g x)
 
+-- 4. Ziplist re-implement the Control.Applicative zippy instance for lists.
+newtype ZipList a = Z [a]
+  deriving (Show)
 
+instance Functor ZipList where
+  -- fmap :: (a -> b) -> ZipList a -> ZipList b
+  fmap g (Z xs) = Z (map g xs)
+
+instance Applicative ZipList where
+  -- pure :: a -> ZipList a
+  pure x = Z (repeat x)
+
+  -- (<*>) :: ZipList (a -> b) -> ZipList a -> ZipList b
+  (Z gs) <*> (Z xs) = Z [g x | (g, x) <- zip gs xs]
+
+-- 5. Work out types for the 4 applicative laws.
